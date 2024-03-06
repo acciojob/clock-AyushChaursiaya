f6ba0bb99a4213a7e0cf50439be5aadab14f5661
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-06 18:47:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateTime() {
+	const now = new Date();
+
+	const date = now.toLocaleDateString();
+	const time = now.toLocaleTimeString();
+
+	const updateDisplay = document.getElementById("time");
+	updateDisplay.textContent = date + ", " + time;
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
